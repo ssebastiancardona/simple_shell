7448c9c419003d60cb956804a2785be8375545b8
MattHenao <2340@holbertonschool.com>
2021-04-12 22:37:33 +0000

    Shell

--- shell.c
+++ shell.c
@@ -1,23 +1,20 @@
 #include "holberton.h"
 int main(void)
 {
-        ssize_t read;
-        char *buff = 0;
-        ssize_t num;
-        while(read != EOF)
-        {
-                /**if(read == EOF)
-                {
-                        free(buff);
-                        exit;
-                }*/
-                read = getline(&buff, &num, stdin);
-                buff[read - 1] = '\0';
-                printf("Antes del tok %s\n", buff);
-                token(buff);
-                printf("Despues del tok %s\n", buff);
-                sleep(2);
-        }
-        free(buff);
-        return (0);
+	ssize_t read;
+	char *buff = 0;
+	ssize_t num;
+	while(read != EOF)
+	{
+		/**if(read == EOF)
+		{
+			free(buff);
+			exit;
+		}*/
+		read = getline(&buff, &num, stdin);
+		buff[read - 1] = '\0';
+		token(buff);
+	}
+	free(buff);
+	return (0);
 }
