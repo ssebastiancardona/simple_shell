9ffa41f946f143c228df2ae537c3bc9e1246059b
MattHenao <2340@holbertonschool.com>
2021-04-14 20:58:37 +0000

    Token

--- token.c
+++ token.c
@@ -8,18 +8,19 @@
 
 void token(char *buff)
 {
-char **token = NULL;
-char *help = NULL;
-int i = 0;
-token = malloc((count(buff) + 1) * sizeof(char *));
-help = strtok(buff, " ");
-while (help != NULL)
-{
-token[i] = help;
-help = strtok(NULL, " ");
-i++;
-}
-token[i] = help;
-execute(token);
-free(token);
+        char **token = NULL;
+        char *help = NULL;
+        int i = 0;
+
+        token = malloc((count(buff) + 1) * sizeof(char *));
+        help = strtok(buff, " ");
+        while (help != NULL)
+        {
+                token[i] = help;
+                help = strtok(NULL, " ");
+                i++;
+        }
+        token[i] = help;
+        execute(token);
+        free(token);
 }
