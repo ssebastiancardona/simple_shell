a9088aa1981c9ea9b898cea3693beb954931fb9a
ssebastiancardona <ssebastiancardona@gmail.com>
2021-04-14 15:37:59 -0500

    betty correction

--- token.c
+++ token.c
@@ -1,19 +1,25 @@
 #include "holberton.h"
+
+/**
+ * token - tokeniza el string
+ * @buff: buffer del getline
+ * Return: nada
+*/
+
 void token(char *buff)
 {
-        char **token = NULL;
-        char *help = NULL;
-        int i = 0;
-
-        token = malloc((count(buff) + 1) * sizeof(char *));
-        help = strtok(buff, " ");
-        while(help != NULL)
-        {
-                token[i] = help;
-                help = strtok(NULL, " ");
-                i++;    
-        }
-        token[i] = help;
-        execute(token);
-        free(token);
+char **token = NULL;
+char *help = NULL;
+int i = 0;
+token = malloc((count(buff) + 1) * sizeof(char *));
+help = strtok(buff, " ");
+while (help != NULL)
+{
+token[i] = help;
+help = strtok(NULL, " ");
+i++;
+}
+token[i] = help;
+execute(token);
+free(token);
 }
