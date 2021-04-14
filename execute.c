9cc07e6f1124750006bed8b38cf9c8b6cae8e574
ssebastiancardona <ssebastiancardona@gmail.com>
2021-04-14 15:40:27 -0500

    betty correction

--- execute.c
+++ execute.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
-
+/**
+ * execute - ejecuta execve en un fork hijo
+ * @token: recibe el **token
+ * Return: 1
+*/
 int execute(char **token)
 {
-        pid_t id;
-
-	id = fork();
-	wait(NULL);
-	if(id == 0)
-	{
-		if(execve(token[0], token, environ) == -1)
-                {
-			perror("Simple_Shell");
-                        return (0);
-                }
-        }
-        return (1);
+pid_t id;
+id = fork();
+wait(NULL);
+if (id == 0)
+{
+if (execve(token[0], token, environ) == -1)
+{
+perror("Simple_Shell");
+return (0);
+}
+}
+return (1);
 }
