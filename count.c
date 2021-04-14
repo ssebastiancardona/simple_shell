74198fb374de52634c4c2b790601bd9d7134461e
MattHenao <2340@holbertonschool.com>
2021-04-14 20:57:41 +0000

    Count

--- count.c
+++ count.c
@@ -6,20 +6,21 @@
 */
 int count(char *buff)
 {
-int estado = 0;
-int contador = 0;
-while (*buff != '\0')
-{
-if (*buff == 32 || *buff == 10 || *buff == 9)
-{
-estado = 0;
-}
-else if (estado == 0)
-{
-estado = 1;
-++contador;
-}
-++buff;
-}
-return (contador);
+	int estado = 0;
+	int contador = 0;
+
+	while (*buff != '\0')
+	{
+		if (*buff == 32 || *buff == 10 || *buff == 9)
+		{
+			estado = 0;
+		}
+		else if (estado == 0)
+		{
+			estado = 1;
+			++contador;
+		}
+		++buff;
+	}
+	return (contador);
 }
