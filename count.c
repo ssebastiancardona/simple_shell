5e78f760503a5e792aae0743337e231f32884c5a
JUANHercs <git>
2021-04-14 00:23:54 +0000

    Bets

--- count.c
+++ count.c
@@ -1,18 +1,23 @@
 #include "holberton.h"
 
+/**
+ *count - counts the lenght
+ *@buff: pointer to the string allocated
+ *Return: int
+ **/
+
 int count(char *buff)
 {
-        int i;
-        int count;
+	int i;
+	int count;
 
-        for(i = 0; buff[i] != '\0'; i++)
-        {
-                if(buff[i] == 32 && buff[i + 1] != 32 && buff[i + 1] != '\0')
-                        count++;
-
-                if(buff[0] != 32)
-                        count++;
-        }
-        return(count);
+	for (i = 0; buff[i] != '\0'; i++)
+	{
+		if (buff[i] == 32 && buff[i + 1] != 32 && buff[i + 1] != '\0')
+			count++;
+		if (buff[0] != 32)
+			count++;
+	}
+	return (count);
 }
 
