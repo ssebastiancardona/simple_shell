366076316f69b9bbb92871be61683ba53a05f78e
ssebastiancardona <ssebastiancardona@gmail.com>
2021-04-14 15:46:06 -0500

    betty fixed

--- main_shell.c
+++ main_shell.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
-
+/**
+ * main - controla modo interactivo y no interactivo
+ * Return: 0
+*/
 int main(void)
 {
-        if(isatty(STDIN_FILENO))
-        {
-                no_interactive();
-        }
-        else
-        {
-                interactive();
-        }
-        return (0);
+if (isatty(STDIN_FILENO))
+{
+no_interactive();
+}
+else
+{
+interactive();
+}
+return (0);
 }
